5f84f2eaa17a579f23c688faf936d3694c91f8d5
master-stm <twisterleader@yahoo.com>
2021-01-01 20:15:51 +0100

    add animation

--- js/script.js
+++ js/script.js
@@ -10,3 +10,19 @@ mySlider = new gaBasicSlider(params);
 let thisYear = document.getElementById('this-year')
 let currentYear = new Date().getFullYear()
 thisYear.innerText = currentYear
+
+let carouslShow = () => anime({
+    targets: '#carousel-container',
+    height: '74vh',
+    easing: 'easeInOutQuad'
+});
+
+let imagesShow = () => anime({
+    targets: '#carousel-container',
+    opacity: 1,
+    delay: 500,
+    easing: 'easeInOutQuad'
+});
+
+carouslShow()
+imagesShow()
